982cd390dc993002ef02f6fd071647bbb6c0216f
Abdulqudus <abdullateefqudusleeq@gmail.com>
2026-02-28 17:09:08 +0100

    Delete app directory

--- app/cart/page.tsx
+++
@@ -1,9 +0,0 @@
-import { supabaseServer } from '@/lib/supabaseServer';
-import CartPageClient from '@/components/CartPageClient';
-
-export default async function CartPage() {
-  // Get current user session server-side
-  const { data: { user } } = await supabaseServer.auth.getUser();
-
-  return <CartPageClient user={user} />;
-}
